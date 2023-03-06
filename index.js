96ed89a5f90e283293f1954fea4c614e4d13869c
Rohit Ingole <ingolerohit2017@gmail.com>
2023-03-06 12:51:57 +0530

    changes

---
+++ index.js
@@ -0,0 +1,19 @@
+var goli = false;
+var biscuit = true;
+var biryani = true;
+var mess = true;
+ if(goli) {
+     console.log("add to basket goli")
+ }
+
+ if(biscuit ){
+     console.log("add to basket biscuit")
+ }
+
+ if(biryani){
+     console.log("add to basket biryani")
+ }
+
+ if(mess) {
+     console.log("add to basket mess")
+ }
